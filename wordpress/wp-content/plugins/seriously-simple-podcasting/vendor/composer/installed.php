d5bfce38b3efef4f10cf25c5ae21dd826194214c
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2024-08-10 11:08:40 +0300

    changes

---
+++ wordpress/wp-content/plugins/seriously-simple-podcasting/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e14aaf6ef0ef63b2f3359ffcdacdc79eec8eaeca',
+        'name' => 'castos/seriously-simple-podcasting',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'castos/seriously-simple-podcasting' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e14aaf6ef0ef63b2f3359ffcdacdc79eec8eaeca',
+            'dev_requirement' => false,
+        ),
+    ),
+);
